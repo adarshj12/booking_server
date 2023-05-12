f37e6f19727ae8ae5fc8f87c6e93c1b1ba2edc0e
adarshj12 <adarshjayadevan12@gmail.com>
2023-05-12 17:18:19 +0530

    jwt

--- controllers/userController.js
+++ controllers/userController.js
@@ -43,7 +43,7 @@ const login = async (req, res) => {
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) return res.status(500).json({ message: 'invalid email or password' });
         if (user.isBlocked) return res.status(203).json({ blocked: true });
-        const token = jwt.sign({ id: user._id, name: user.username, mobile: user.mobile, user: true }, process.env.SECRET);
+        const token = jwt.sign({ id: user._id, name: user.username, mobile: user.mobile, user: true }, process.env.SECRET,{ expiresIn: "10h" });
         res.status(202)
             .json({ message: 'login successful', token })
     } catch (error) {
@@ -55,7 +55,7 @@ const getOTP = async (req, res) => {
     try {
         let user = await User.findOne({ mobile: req.params.id });
         if (user) {
-            const token = jwt.sign({ id: user._id, name: user.username, mobile: user.mobile, user: true }, process.env.SECRET);
+            const token = jwt.sign({ id: user._id, name: user.username, mobile: user.mobile, user: true }, process.env.SECRET,{ expiresIn: "10h" });
             return res.status(202).json({ message: 'user exist', token })
         }
         res.status(203).json({ message: "mobile no. mismatch" })
@@ -78,7 +78,7 @@ const googleAuth = async (req, res) => {
                 password
             })
             await newUser.save();
-            const token = jwt.sign({ id: newUser._id, name: newUser.username, user: true }, process.env.SECRET);
+            const token = jwt.sign({ id: newUser._id, name: newUser.username, user: true }, process.env.SECRET,{ expiresIn: "10h" });
             return res.status(202).json({ message: 'login successful', token })
         } else {
             if (userExist.isBlocked) return res.status(203).json({ blocked: true });
